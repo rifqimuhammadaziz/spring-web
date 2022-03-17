a5ebf4c6037c66fe5b0550ba9062a1ce9a1878cb
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-17 13:51:22 +0700

    add Bootstrap using webjars

--- src/main/java/rifqimuhammadaziz/springweb/controllers/HomeController.java
+++ src/main/java/rifqimuhammadaziz/springweb/controllers/HomeController.java
@@ -1,5 +1,18 @@
 package rifqimuhammadaziz.springweb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("")
 public class HomeController {
+
+    @GetMapping
+    public String home(Model model) {
+        String title = "Welcome to Spring Application";
+        model.addAttribute("title", title);
+        return "index";
+    }
 }
