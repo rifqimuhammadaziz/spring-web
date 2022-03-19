8aff7273777caf3b240e469f61ddcbcb468e2036
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-19 09:46:09 +0700

    handling form submission

--- src/main/java/rifqimuhammadaziz/springweb/controllers/HomeController.java
+++ src/main/java/rifqimuhammadaziz/springweb/controllers/HomeController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rifqimuhammadaziz.springweb.entity.Product;
 import rifqimuhammadaziz.springweb.service.ProductService;
 
 @Controller
@@ -20,5 +22,18 @@ public class HomeController {
         model.addAttribute("title", title);
         model.addAttribute("products", productService.findAll());
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String viewAddProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "product/add";
+    }
+
+    @PostMapping("/save")
+    public String saveProduct(Product product, Model model) {
+        productService.addProduct(product);
+
+        return "redirect:/";
     }
 }
--- src/main/java/rifqimuhammadaziz/springweb/service/ProductService.java
+++ src/main/java/rifqimuhammadaziz/springweb/service/ProductService.java
@@ -3,21 +3,29 @@ package rifqimuhammadaziz.springweb.service;
 import org.springframework.stereotype.Service;
 import rifqimuhammadaziz.springweb.entity.Product;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    private static List<Product> products = Arrays.asList(
-            new Product(1L, "0001", "iPhone", 5000000.0),
-            new Product(2L, "0002", "Samsung", 5000000.0),
-            new Product(3L, "0003", "Realme", 5000000.0),
-            new Product(4L, "0004", "Xiaomi", 5000000.0),
-            new Product(5L, "0005", "Oppo", 5000000.0)
-    );
+    private static List<Product> products = new ArrayList<Product>() {
+        {
+            add(new Product(System.currentTimeMillis(), "0001", "iPhone", 5000000.0));
+            add(new Product(System.currentTimeMillis(), "0002", "Samsung", 5000000.0));
+            add(new Product(System.currentTimeMillis(), "0003", "Realme", 5000000.0));
+            add(new Product(System.currentTimeMillis(), "0004", "Xiaomi", 5000000.0));
+            add(new Product(System.currentTimeMillis(), "0005", "Oppo", 5000000.0));
+        }
+    };
 
     public List<Product> findAll() {
         return products;
+    }
+
+    public void addProduct(Product product) {
+        product.setId(System.currentTimeMillis()); // create ID from current time
+        products.add(product);
     }
 }
